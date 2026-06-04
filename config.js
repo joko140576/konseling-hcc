c4053f966aab0f6b8f5ac1edd0b16358c1ed8ec7
joko prabowo <prabowo.joko2019@gmail.com>
2026-06-04 11:02:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
    Script Anda setelah di-deploy.
    ============================================= */
 
-var GAS_URL = "GANTI_DENGAN_URL_APPS_SCRIPT_ANDA";
+var GAS_URL = "https://script.google.com/macros/s/AKfycbytgxZg4A9sJQ64rowJsZvgXwWHOdZSlYk1trlXUMu5VuN8DR4SPhl_5eh_iIcsMJpi/exec";
 // Contoh: "https://script.google.com/macros/s/AKfycb.../exec"
