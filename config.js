ae070b7396b1df0a0d7f77fca8e371a205073676
joko prabowo <prabowo.joko2019@gmail.com>
2026-06-11 19:33:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
    Script Anda setelah di-deploy.
    ============================================= */
 
-var GAS_URL = "https://script.google.com/macros/s/AKfycbytgxZg4A9sJQ64rowJsZvgXwWHOdZSlYk1trlXUMu5VuN8DR4SPhl_5eh_iIcsMJpi/exec";
+var GAS_URL = "https://script.google.com/macros/s/AKfycbz5DlFI6lmLTl8wCUGb_O0Jy2sfYzxWnWHqtYQPJ6DR90iLbYGaO-JZjA3dUz2iXoxA/exec";
 // Contoh: "https://script.google.com/macros/s/AKfycb.../exec"
