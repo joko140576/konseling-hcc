131a9657bff07b98400272478274a2b89bc84568
joko prabowo <prabowo.joko2019@gmail.com>
2026-06-11 19:29:53 +0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-/* =============================================
-   HCC KONSELING — KONFIGURASI
-   Ganti URL di bawah dengan URL Google Apps
-   Script Anda setelah di-deploy.
-   ============================================= */
-
-var GAS_URL = "https://script.google.com/macros/s/AKfycbx1ELli55mZZ8I8To_oTBzubYEKzF9baAY-7vvSnJPS3twpPS_Cdc3WvTL9ajiPaNU1/exec";
-// Contoh: "https://script.google.com/macros/s/AKfycb.../exec"
